16f8adf8aeb719ce9cc2369fdb566b49fa6d2172
unknown <ys8763504@gmail.com>
2026-04-16 16:57:34 +0530

    few changes

--- finsight/settings.py
+++ finsight/settings.py
@@ -73,13 +73,18 @@ WSGI_APPLICATION = 'finsight.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config()
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
